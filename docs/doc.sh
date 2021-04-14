772c9646c02a067ea1035303b939c39baebefae0
Abhishek Chatterjee imdeepmind <abhishek.chatterjee97@protonmail.com>
2021-04-14 23:36:11 +0530

    added deployment script

---
+++ docs/doc.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+echo "--> Building rst files"
+sphinx-apidoc -o ./ ../pcy/
+
+echo "--> Building HTML files"
+make html
+
+echo "--> Moving the HTML files to the correct directory"
+cp -r ./_build/html/* ./
