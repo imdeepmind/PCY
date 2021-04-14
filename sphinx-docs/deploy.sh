34ac1ddb07151300e887fe8c3752ee64745d8286
Abhishek Chatterjee imdeepmind <abhishek.chatterjee97@protonmail.com>
2021-04-15 00:01:25 +0530

    added .nojekyll file

--- sphinx-docs/deploy.sh
+++ sphinx-docs/deploy.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
-echo "--> Building rst files"
 sphinx-apidoc -o ./ ../pcy/
-
-echo "--> Building HTML files"
 make html
-
-echo "--> Moving the files to correct folder"
 cp -r ./_build/html/* ../docs/
+touch ../docs/.nojekyll
